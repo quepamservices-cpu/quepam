3ab91a31845fa8cb5fa3fb3eaa314c20d11d1744
quepamservices-cpu <que.pamservices@gmail.com>
2026-09-20 20:17:00 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
-import { Text, View } from 'react-native';
+import { View, Text } from 'react-native';
 export default function App() {
-  return <View style={{flex:1,justifyContent:'center',alignItems:'center'}}><Text>QuePam</Text></View>
+  return (
+    <View style={{flex:1, backgroundColor:'#000', justifyContent:'center', alignItems:'center'}}>
+      <Text style={{color:'#fff', fontSize:24, fontWeight:'bold'}}>QuePam Works!</Text>
+    </View>
+  );
 }
